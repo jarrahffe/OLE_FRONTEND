bf215c86cdeec38d3e462b72dcdc1bec959edbfb
Tim <88568150+timli3@users.noreply.github.com>
2026-06-05 11:47:20 +1000

    Update Settings.ts

--- OLE_FRONTEND/src/config/Settings.ts
+++ OLE_FRONTEND/src/config/Settings.ts
@@ -1,16 +1,16 @@
 import moment from "moment";
-export const START_SEMESTER = "2026-02-23";
+export const START_SEMESTER = "2026-06-1";
 export const SEMESTER = 1;
 
 export const CURRENT_WEEK = moment().diff(moment(START_SEMESTER, "YYYY-MM-DD"), "weeks");
-export const NUM_WEEKS_TOTAL = 24;
+export const NUM_WEEKS_TOTAL = 16;
 export const TEACHER_NAME = "Ole";
 export let SESSION =  'Semester Break' || `Semester ${SEMESTER}` || 'Exam Period';
 
 
-if (0 <= CURRENT_WEEK && CURRENT_WEEK < 24) {
+if (0 <= CURRENT_WEEK && CURRENT_WEEK < 14) {
     SESSION = `Semester ${SEMESTER}`;
-} else if (0 < CURRENT_WEEK && CURRENT_WEEK < 24) {
+} else if (0 < CURRENT_WEEK && CURRENT_WEEK < 16) {
     SESSION = 'Exam Period'
 } else {
     SESSION = 'Semester Break'
